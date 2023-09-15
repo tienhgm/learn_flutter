c2dffb3c9f7f56a2cf42eea520ce0f588cb2ca3b
MIC\tiennm01 <tiennm01@mic.vn>
2023-09-15 14:57:46 +0700

    fix ui result screen

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -12,25 +12,26 @@ class QuestionsSummary extends StatelessWidget {
             children: summaryData.map((data) {
           return Row(
               crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Container(
-                    height: 20,
-                    width: 20,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: data['user_answer'] == data['correct_answer']
-                            ? Colors.blue[400]
-                            : Colors.red[400]),
-                    child: Center(
-                      child: Text(
-                        ((data['question_index'] as int) + 1).toString(),
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    )),
-                const SizedBox(width: 8),
+                  height: 20,
+                  width: 20,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: data['user_answer'] == data['correct_answer']
+                          ? Colors.blue[400]
+                          : Colors.red[400]),
+                  child: Text(
+                    ((data['question_index'] as int) + 1).toString(),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+                const SizedBox(width: 16),
                 Expanded(
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
                         data['question'] as String,
