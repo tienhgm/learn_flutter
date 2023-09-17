4eba6a41219481d49597b12c76e8a0b589c627a0
tiennguyen <tienhgm@gmail.com>
2023-09-17 19:30:08 +0700

    enum:

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,16 +11,26 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter course',
+        amount: 20.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Workbook',
+        amount: 14.99,
+        date: DateTime.now(),
+        category: Category.work),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
-            Text('The chart'),
-            Text('Expenses list...'),
-          ],
-        ),
+    return const Scaffold(
+      body: Column(
+        children: [
+          Text('The chart'),
+          Text('Expenses list...'),
+        ],
       ),
     );
   }
