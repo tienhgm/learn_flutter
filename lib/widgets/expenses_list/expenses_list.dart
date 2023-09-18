e522b43a0a68b2d060e33bf0527acdc6a72a9755
tiennguyen <tienhgm@gmail.com>
2023-09-18 22:28:28 +0700

    adding snackbar

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -3,13 +3,20 @@ import 'package:flutter_app/models/expense.dart';
 import 'package:flutter_app/widgets/expenses_list/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
+  final void Function(Expense expense) onRemoveExpense;
   final List<Expense> expenses;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
         physics: const ScrollPhysics(parent: BouncingScrollPhysics()),
         itemCount: expenses.length,
-        itemBuilder: (context, idx) => ExpenseItem(expenses[idx]));
+        itemBuilder: (context, idx) => Dismissible(
+            key: ValueKey(expenses[idx]),
+            onDismissed: (direction) {
+              onRemoveExpense(expenses[idx]);
+            },
+            child: ExpenseItem(expenses[idx])));
   }
 }
